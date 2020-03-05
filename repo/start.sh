28ab665085e7fa9433cd76dc2a89b10ab90dc042
hanxianzhai <hanxianzhai@gmail.com>
2020-03-05 17:42:31 +0800

    v0.6

--- repo/start.sh
+++ repo/start.sh
@@ -7,7 +7,7 @@ reposync -n --repoid=extras7 --repoid=updates7 --repoid=base7 -p /data/repo/Cent
 reposync -n --repoid=extras8 --repoid=AppStream8 --repoid=base8 -p /data/repo/CentOS/8/x86_64/
 reposync -n --repoid=centos-7-epel -p /data/repo/CentOS/epel/7/x86_64/
 reposync -n --repoid=centos-8-epel --repoid=centos-8-epel-modular -p /data/repo/CentOS/epel/8/x86_64/
-reposync -n --repoid=docker-ce-stable --repoid=docker-ce-edge -p /data/repo/CentOS/docker
+reposync -n --repoid=docker-ce-stable --repoid=docker-ce-edge -p /data/repo/CentOS/docker/x86_64/
 echo 数据同步结束
 
 #更新元数据
@@ -22,5 +22,5 @@ createrepo --update /data/repo/CentOS/8/x86_64/extras/
 createrepo --update /data/repo/CentOS/8/x86_64/AppStream/
 createrepo --update /data/repo/CentOS/epel/8/x86_64/epel/
 createrepo --update /data/repo/CentOS/epel/8/x86_64/epel-modular/
-createrepo --update /data/repo/CentOS/7/x86_64/docker-ce-stable/
-createrepo --update /data/repo/CentOS/7/x86_64/docker-ce-edge/
+createrepo --update /data/repo/CentOS/x86_64/docker-ce-stable/
+createrepo --update /data/repo/CentOS/x86_64/docker-ce-edge/
