03a50605c00c31ae14f9eb1ac78e66927997567f
hanxianzhai <hanxianzhai@gmail.com>
2020-03-05 17:10:52 +0800

    v0.5

--- repo/start.sh
+++ repo/start.sh
@@ -7,7 +7,6 @@ reposync -n --repoid=extras7 --repoid=updates7 --repoid=base7 -p /data/repo/Cent
 reposync -n --repoid=extras8 --repoid=AppStream8 --repoid=base8 -p /data/repo/CentOS/8/x86_64/
 reposync -n --repoid=centos-7-epel -p /data/repo/CentOS/epel/7/x86_64/
 reposync -n --repoid=centos-8-epel --repoid=centos-8-epel-modular -p /data/repo/CentOS/epel/8/x86_64/
-
 reposync -n --repoid=docker-ce-stable --repoid=docker-ce-edge -p /data/repo/CentOS/docker
 echo 数据同步结束
 
@@ -17,11 +16,11 @@ echo 开始更新元数据
 createrepo --update /data/repo/CentOS/7/x86_64/base/
 createrepo --update /data/repo/CentOS/7/x86_64/extras/
 createrepo --update /data/repo/CentOS/7/x86_64/updates/
-createrepo --update /data/repo/CentOS/7/x86_64/epel/
+createrepo --update /data/repo/CentOS/epel/7/x86_64/
 createrepo --update /data/repo/CentOS/8/x86_64/base/
 createrepo --update /data/repo/CentOS/8/x86_64/extras/
 createrepo --update /data/repo/CentOS/8/x86_64/AppStream/
-createrepo --update /data/repo/CentOS/8/x86_64/epel/
-createrepo --update /data/repo/CentOS/8/x86_64/epel-modular/
+createrepo --update /data/repo/CentOS/epel/8/x86_64/epel/
+createrepo --update /data/repo/CentOS/epel/8/x86_64/epel-modular/
 createrepo --update /data/repo/CentOS/7/x86_64/docker-ce-stable/
 createrepo --update /data/repo/CentOS/7/x86_64/docker-ce-edge/
